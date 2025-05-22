a33e78077d37041f4addb016a33322c140352eb7
sarahvue <sarahvue0924@gmail.com>
2025-05-21 21:13:15 -0700

    Solution file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+let burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+function addBurger() {
+  let newBurger = "Flatburger";
+  burgers.push(newBurger);
+}
+
+if (true) {
+  let anothernewBurger = "Maple Bacon Burger";
+  burgers.push(anothernewBurger);
+
+  function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake";
+  }
+}
